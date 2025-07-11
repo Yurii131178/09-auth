376edf6f87639fb6091da0a14e6387e538308c1c
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-07-11 12:05:55 +0300

    add metadata to Profile page

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -1,5 +1,29 @@
 import Link from 'next/link';
 import css from './Profile.module.css';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Your Profile - NoteHub',
+  description:
+    'View and manage your personal profile details and settings on NoteHub',
+  keywords: ['profile', 'user settings', 'account', 'personal info', 'NoteHub'],
+  openGraph: {
+    title: 'Your Profile - NoteHub',
+    description:
+      'View and manage your personal profile details and settings on NoteHub',
+    url: 'https://08-zustand-theta.vercel.app/notes/action/create', // replace with 09-auth, once deployed on Vercel
+    images: [
+      {
+        url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+        alt: 'NoteHub - Note Management App Logo',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'en_GB',
+    type: 'website',
+  },
+};
 
 const Profile = () => {
   return (
@@ -7,7 +31,7 @@ const Profile = () => {
       <div className={css.profileCard}>
         <div className={css.header}>
           <h1 className={css.formTitle}>Profile Page</h1>
-          <Link href={'#'} className={css.editProfileButton}>
+          <Link href={'/profile/edit'} className={css.editProfileButton}>
             Edit Profile
           </Link>
         </div>
