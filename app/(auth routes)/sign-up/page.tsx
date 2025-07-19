bed2b8d7cc325437a4211848843fca01780651f5
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-07-19 19:15:05 +0300

    add authSore for Zustand_fix minor issues in log/reg

--- app/(auth routes)/sign-up/page.tsx
+++ app/(auth routes)/sign-up/page.tsx
@@ -1,5 +1,69 @@
-const Register = () => {
-  return <div>Register works</div>;
+'use client';
+
+import React, { useState } from 'react';
+import css from './SignUpPage.module.css';
+import { useRouter } from 'next/navigation';
+import { getMe, register, RegisterRequest } from '@/lib/api/clientApi';
+import { useAuthStore } from '@/lib/store/authStore';
+
+const SignUp = () => {
+  const router = useRouter();
+  const [error, setError] = useState('');
+  const { setIsAuthenticated, setUser } = useAuthStore.getState();
+
+  const handleSubmit = async (formData: FormData) => {
+    try {
+      const formValues: RegisterRequest = {
+        email: String(formData.get('email')),
+        password: String(formData.get('password')),
+      };
+      await register(formValues);
+      setIsAuthenticated(true);
+      const user = await getMe();
+      setUser(user);
+      router.push('/profile');
+    } catch (err) {
+      console.error('error', err);
+      setError('Invalid email or password');
+    }
+  };
+
+  return (
+    <main className={css.mainContent}>
+      <form action={handleSubmit} className={css.form}>
+        <h1 className={css.formTitle}>Sign up</h1>
+        <div className={css.formGroup}>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            className={css.input}
+            required
+          />
+        </div>
+
+        <div className={css.formGroup}>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            className={css.input}
+            required
+          />
+        </div>
+
+        <div className={css.actions}>
+          <button type="submit" className={css.submitButton}>
+            Register
+          </button>
+        </div>
+
+        {error && <p className={css.error}>{error}</p>}
+      </form>
+    </main>
+  );
 };
 
-export default Register;
+export default SignUp;
