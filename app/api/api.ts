14bded6eff359d90ccb2cf3f8c6accad02b51b1f
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-07-20 14:32:21 +0300

    rewrite the routes

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,3 +1,5 @@
+//app/api/api.ts
+
 import axios from 'axios';
 
 export const api = axios.create({
--- lib/api/api.ts
+++ lib/api/api.ts
@@ -7,5 +7,5 @@ const baseURL = process.env.NEXT_PUBLIC_API_URL + '/api';
 
 export const nextServer = axios.create({
   baseURL,
-  withCredentials: true, // дозволяє axios працювати з cookie
+  withCredentials: true,
 });
